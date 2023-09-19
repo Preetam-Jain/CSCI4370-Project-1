519589f34afe46328dfea64098f112ac640f8d31
Preetam Jain <ppj36825@uga.edu>
2023-09-19 18:15:51 -0400

    updated relation to contain structure of table

--- CSCI4370 Project 1/uga/cs4370/mydb/Database.java
+++ CSCI4370 Project 1/uga/cs4370/mydb/Database.java
@@ -5,12 +5,20 @@ import java.util.ArrayList;
 
 public class Database {
     
-    List<Relation> relations;
-    String name;
+    private List<Relation> relations;
+    private String name;
 
     public Database(String name) {
         this.name = name;
         this.relations = new ArrayList<Relation>();
     }
-    
+
+    public List<Relation> getRelations() {
+        return this.relations;
+    }
+
+    public String getName() {
+        return name;
+    }
+
 }
--- CSCI4370 Project 1/uga/cs4370/mydb/impl/Relation.java
+++ CSCI4370 Project 1/uga/cs4370/mydb/impl/Relation.java
@@ -1,43 +1,70 @@
 package uga.cs4370.mydb.impl;
 
 import java.util.List;
+import java.util.ArrayList;
+
 import uga.cs4370.mydb.Cell;
 import uga.cs4370.mydb.Type;
 
 public class Relation implements uga.cs4370.mydb.Relation {
 
+    private String name; // table name
+    private int size = 0; // ammount of rows in the table
+    private List<List<Cell>> table; // contains the structure for the table 
+    private List<String> attrs; // the names of the attributes of the table
+    private List<Type> types; // the names of the types of each column
+
+    /*
+    * Constructor for a table/relation, takes in name, all of the names of the attributes,
+    * and the names of the types of each column. The parameters receive the arguments from
+    * the main class. However, the overall storage is only created within the constructor,
+    * not the driver.
+    */
+    public Relation(String name, ArrayList<String> attrs, List<Type> types) {
+        this.name = name;
+        this.attrs = attrs;
+        this.types = types;
+        this.table = new ArrayList<List<Cell>>(); // creates the storage of this array 
+    }
+
     public String getName() {
-        throw new UnsupportedOperationException("Method not implemented yet");
+        return this.name;
     }
 
     @Override
     public int getSize() {
-        throw new UnsupportedOperationException("Method not implemented yet");
+        return this.size;
     }
 
     @Override
     public List<List<Cell>> getRows() {
-        throw new UnsupportedOperationException("Method not implemented yet");
+        return this.table;
     }
 
     @Override
     public List<Type> getTypes() {
-        throw new UnsupportedOperationException("Method not implemented yet");
+        return this.types;
     }
 
     @Override
     public List<String> getAttrs() {
-        throw new UnsupportedOperationException("Method not implemented yet");
+        return this.attrs;
     }
 
     @Override
     public boolean hasAttr(String attr) {
-        throw new UnsupportedOperationException("Method not implemented yet");
+        if (this.attrs.contains(attr)) {
+            return true;
+        } 
+        throw new IllegalArgumentException("Does not contain the specified attribute");
     }
 
     @Override
     public int getAttrIndex(String attr) {
-        throw new UnsupportedOperationException("Method not implemented yet");
+        if (this.hasAttr(attr)) {
+            return this.attrs.indexOf(attr);
+        }
+        throw new IllegalArgumentException("Does not contain the specified attribute");
     }
 
     @Override
