bdb8929b12d9a8df24e34b0894d3bdd5b50e5807
jannu144 <ssatish53@icloud.com>
2023-09-21 23:24:31 -0400

    predicate implementation (a skeleton for now)

---
+++ CSCI4370 Project 1/uga/cs4370/mydb/impl/Predicate.java
@@ -0,0 +1,12 @@
+package uga.cs4370.mydb.impl;
+
+import uga.cs4370.mydb.Cell;
+import java.util.List;
+public class Predicate implements uga.cs4370.mydb.Predicate {
+    
+    @Override
+    public boolean check(List<Cell> row){
+        /* */
+        return false;
+    }
+}
